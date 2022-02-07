869e59fb05dacdf6945b1c01d0a9e525e9c322bc
BBlack-Hun <h1103j@kangwon.ac.kr>
2022-02-07 10:35:22 +0900

    조건부 렌더링

--- src/App.js
+++ src/App.js
@@ -6,7 +6,8 @@ import './App.css';
 function App() {
   return (
     <Wrapper>
-      <Hello name="react" color="red" />
+      <Hello name="react" color="red" isSpecial />
+      {/* Boolean 변수를 사용할때, 변수 자체만 사용해도  true 값을 반홚나다. */}
       <Hello color="pink" />
     </Wrapper>
   );
--- src/Hello.js
+++ src/Hello.js
@@ -1,7 +1,14 @@
 import React from 'react';
 
-function Hello({ color, name }) {
-  return <div style={{ color }}>안녕하세요! {name}</div>;
+function Hello({ color, name, isSpecial }) {
+  return (
+    <div style={{ color }}>
+      {/* {isSpecial ? <b>*</b> : null} */}
+      {isSpecial && <b>*</b>}
+      {/* <b>{isSpecial ? '스페셜하다!' : '낫스페셜하다'}</b> */}
+      안녕하세요! {name}
+    </div>
+  );
 }
 
 Hello.defaultProps = {
